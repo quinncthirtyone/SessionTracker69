17cf4a68b6059c1c220277d81c63ea0bae6f4994
quinncthirtyone <conorquinn2704@gmail.com>
2025-09-02 23:40:25 +0100

    Red Delete and Name Profiles
    Idle detection and edit game updates are super broken

--- modules/SettingsFunctions.psm1
+++ modules/SettingsFunctions.psm1
@@ -39,31 +39,38 @@ function RenderEditGameForm($GamesList) {
             FilterListBox -filterText $textSearch.Text -listBox $listBox -originalItems $GamesList
         })
 
+    $changeEventHandler = { $buttonOK.Enabled = $true }
+
     $labelName = Createlabel "Name:" 170 20; $editGameForm.Controls.Add($labelName)
-    $textName = CreateTextBox "" 245 20 300 20;	$editGameForm.Controls.Add($textName)
+    $textName = CreateTextBox "" 245 20 200 20;	$editGameForm.Controls.Add($textName)
+    $textName.Add_TextChanged($changeEventHandler)
 
     $labelExe = Createlabel "Exe:" 170 60; $editGameForm.Controls.Add($labelExe)
     $textExe = CreateTextBox "" 245 60 200 20; $editGameForm.Controls.Add($textExe)
+    $textExe.Add_TextChanged($changeEventHandler)
 
     $labelPlatform = Createlabel "Platform:" 170 100; $editGameForm.Controls.Add($labelPlatform)
     $textPlatform = CreateTextBox "" 245 100 200 20; $editGameForm.Controls.Add($textPlatform)
+    $textPlatform.Add_TextChanged($changeEventHandler)
 
     $labelPlayTime = Createlabel "PlayTime:" 170 140; $editGameForm.Controls.Add($labelPlayTime)
     $textPlayTime = CreateTextBox "" 245 140 200 20; $editGameForm.Controls.Add($textPlayTime)
+    $textPlayTime.Add_TextChanged($changeEventHandler)
 
     $checkboxCompleted = New-Object Windows.Forms.CheckBox
     $checkboxCompleted.Text = "Finished"
-    $checkboxCompleted.Top = 135
+    $checkboxCompleted.Top = 95
     $checkboxCompleted.Left = 470
+    $checkboxCompleted.Add_CheckedChanged($changeEventHandler)
     $editGameForm.Controls.Add($checkboxCompleted)
 
     $checkboxDropped = New-Object Windows.Forms.CheckBox
     $checkboxDropped.Text = "Dropped"
-    $checkboxDropped.Top = 155
+    $checkboxDropped.Top = 115
     $checkboxDropped.Left = 470
+    $checkboxDropped.Add_CheckedChanged($changeEventHandler)
     $checkboxDropped.Add_CheckedChanged({
             if ($checkboxDropped.Checked) {
-                $checkboxCompleted.Checked = $true
                 $checkboxCompleted.Enabled = $false
                 $checkboxHold.Checked = $false
                 $checkboxForever.Checked = $false
@@ -78,11 +85,11 @@ function RenderEditGameForm($GamesList) {
 
     $checkboxHold = New-Object Windows.Forms.CheckBox
     $checkboxHold.Text = "Pick Up Later"
-    $checkboxHold.Top = 175
+    $checkboxHold.Top = 135
     $checkboxHold.Left = 470
+    $checkboxHold.Add_CheckedChanged($changeEventHandler)
     $checkboxHold.Add_CheckedChanged({
             if ($checkboxHold.Checked) {
-                $checkboxCompleted.Checked = $true
                 $checkboxCompleted.Enabled = $false
                 $checkboxDropped.Checked = $false
                 $checkboxForever.Checked = $false
@@ -97,11 +104,11 @@ function RenderEditGameForm($GamesList) {
 
     $checkboxForever = New-Object Windows.Forms.CheckBox
     $checkboxForever.Text = "Forever Game"
-    $checkboxForever.Top = 195
+    $checkboxForever.Top = 155
     $checkboxForever.Left = 470
+    $checkboxForever.Add_CheckedChanged($changeEventHandler)
     $checkboxForever.Add_CheckedChanged({
             if ($checkboxForever.Checked) {
-                $checkboxCompleted.Checked = $true
                 $checkboxCompleted.Enabled = $false
                 $checkboxHold.Checked = $false
                 $checkboxDropped.Checked = $false
@@ -114,11 +121,20 @@ function RenderEditGameForm($GamesList) {
         })
     $editGameForm.Controls.Add($checkboxForever)
 
+    $checkboxIdleDetection = New-Object Windows.Forms.CheckBox
+    $checkboxIdleDetection.Text = "Idle Detection"
+    $checkboxIdleDetection.Top = 175
+    $checkboxIdleDetection.Left = 470
+    $checkboxIdleDetection.Checked = $true
+    $checkboxIdleDetection.Add_CheckedChanged($changeEventHandler)
+    $editGameForm.Controls.Add($checkboxIdleDetection)
+
     $labelPictureBox = Createlabel "Game Icon" 57 165; $editGameForm.Controls.Add($labelPictureBox)
     $pictureBox = CreatePictureBox $imagePath 15 20 140 140
     $editGameForm.Controls.Add($pictureBox)
 
     $listBox.Add_SelectedIndexChanged({
+            $buttonOK.Enabled = $false
             $selectedGame = GetGameDetails $listBox.SelectedItem
 
             $textName.Text = $selectedGame.name
@@ -133,6 +149,8 @@ function RenderEditGameForm($GamesList) {
             if ($checkboxForever.Checked -or $checkboxHold.Checked -or $checkboxDropped.Checked) {
                 $checkboxCompleted.Enabled = $false
             }
+
+            $checkboxIdleDetection.Checked = ($selectedGame.idle_detection -eq 1)
 
             $textPlayTime.Text = PlayTimeMinsToString $selectedGame.play_time
 
@@ -184,6 +202,7 @@ function RenderEditGameForm($GamesList) {
                 $pictureBox.Image.Dispose()
                 $pictureBox.Image = [System.Drawing.Image]::FromFile($imagePath)
                 $openFileDialog.Dispose()
+                $buttonOK.Enabled = $true
             }
         })
     $editGameForm.Controls.Add($buttonUpdateIcon)
@@ -199,7 +218,13 @@ function RenderEditGameForm($GamesList) {
         })
     $editGameForm.Controls.Add($buttonUpdateExe)
 
-    $buttonRemove = CreateButton "Delete" 470 100
+    $buttonRemove = CreateButton "Delete" 470 20
+    $buttonRemove.Size = New-Object System.Drawing.Size(75, 23)
+    $buttonRemove.BackColor = [System.Drawing.Color]::Firebrick
+    $buttonRemove.ForeColor = [System.Drawing.Color]::White
+    $buttonRemove.FlatStyle = 'flat'
+    $buttonRemove.FlatAppearance.BorderSize = 0
+    $buttonRemove.FlatAppearance.MouseOverBackColor = [System.Drawing.Color]::DarkRed
     $buttonRemove.Add_Click({
             $gameName = $textName.Text
 
@@ -222,6 +247,7 @@ function RenderEditGameForm($GamesList) {
     $editGameForm.Controls.Add($buttonRemove)
 
     $buttonOK = CreateButton "OK" 245 190
+    $buttonOK.Enabled = $false
     $buttonOK.Add_Click({
             $currentlySelectedIndex = $listBox.SelectedIndex
 
@@ -243,16 +269,17 @@ function RenderEditGameForm($GamesList) {
 
             $gameExeName = $textExe.Text -replace ".exe"
 
-            $gameCompleteStatus = if ($checkboxCompleted.Checked) { "TRUE" } else { "FALSE" }
-
             $gameStatus = ""
-            if ($checkboxDropped.Checked) { $gameStatus = "dropped"; $checkboxCompleted.Checked = $true; $gameCompleteStatus = "TRUE"; }
-            if ($checkboxHold.Checked) { $gameStatus = "hold"; $checkboxCompleted.Checked = $true; $gameCompleteStatus = "TRUE"; }
-            if ($checkboxForever.Checked) { $gameStatus = "forever"; $checkboxCompleted.Checked = $true; $gameCompleteStatus = "TRUE"; }
+            if ($checkboxDropped.Checked) { $gameStatus = "dropped" }
+            elseif ($checkboxHold.Checked) { $gameStatus = "hold" }
+            elseif ($checkboxForever.Checked) { $gameStatus = "forever" }
 
-            UpdateGameOnEdit -OriginalGameName $textOriginalGameName.Text -GameName $gameName -GameExeName $gameExeName -GameIconPath $pictureBoxImagePath.Text -GamePlayTime $playTimeInMin -GameCompleteStatus $gameCompleteStatus -GamePlatform $textPlatform.Text -GameStatus $gameStatus
+            $gameCompleteStatus = if ($checkboxCompleted.Checked -or $gameStatus -ne "") { "TRUE" } else { "FALSE" }
+
+            UpdateGameOnEdit -OriginalGameName $textOriginalGameName.Text -GameName $gameName -GameExeName $gameExeName -GameIconPath $pictureBoxImagePath.Text -GamePlayTime $playTimeInMin -GameCompleteStatus $gameCompleteStatus -GamePlatform $textPlatform.Text -GameStatus $gameStatus -GameIdleDetection $checkboxIdleDetection.Checked
 
             ShowMessage "Updated '$gameName' in Database." "OK" "Asterisk"
+            $buttonOK.Enabled = $false
 
             # Clear existing and then pre load image in ui\resources\images folder for rendering 'All Games' list faster
             $imageFileName = ToBase64 $gameName
@@ -392,6 +419,12 @@ function RenderEditPlatformForm($PlatformsList) {
     $editPlatformForm.Controls.Add($buttonClearExe)
 
     $buttonRemove = CreateButton "Delete" 300 18
+    $buttonRemove.Size = New-Object System.Drawing.Size(75, 23)
+    $buttonRemove.BackColor = [System.Drawing.Color]::Firebrick
+    $buttonRemove.ForeColor = [System.Drawing.Color]::White
+    $buttonRemove.FlatStyle = 'flat'
+    $buttonRemove.FlatAppearance.BorderSize = 0
+    $buttonRemove.FlatAppearance.MouseOverBackColor = [System.Drawing.Color]::DarkRed
     $buttonRemove.Add_Click({
             $platformName = $textName.Text
 
@@ -649,6 +682,12 @@ function RenderGamingPCForm($PCList) {
 
     $buttonUpdate = CreateButton "Update" 265 190
     $buttonRemove = CreateButton "Delete" 360 150
+    $buttonRemove.Size = New-Object System.Drawing.Size(75, 23)
+    $buttonRemove.BackColor = [System.Drawing.Color]::Firebrick
+    $buttonRemove.ForeColor = [System.Drawing.Color]::White
+    $buttonRemove.FlatStyle = 'flat'
+    $buttonRemove.FlatAppearance.BorderSize = 0
+    $buttonRemove.FlatAppearance.MouseOverBackColor = [System.Drawing.Color]::DarkRed
 
     $listBox.Add_SelectedIndexChanged({
             if ($null -eq $listBox.SelectedItem) { return }
@@ -972,3 +1011,36 @@ function RenderAddPlatformForm() {
     $addPlatformForm.Dispose()
 }
 
+function RenderProfileSettingsForm() {
+    $profileSettingsForm = CreateForm "Gaming Gaiden: Name Profiles" 300 180 ".\icons\running.ico"
+
+    $profiles = Get-Profiles
+
+    $labelProfile1 = Createlabel "Profile 1:" 20 20
+    $profileSettingsForm.Controls.Add($labelProfile1)
+    $textProfile1 = CreateTextBox $profiles[0].name 100 20 150 20
+    $profileSettingsForm.Controls.Add($textProfile1)
+
+    $labelProfile2 = Createlabel "Profile 2:" 20 60
+    $profileSettingsForm.Controls.Add($labelProfile2)
+    $textProfile2 = CreateTextBox $profiles[1].name 100 60 150 20
+    $profileSettingsForm.Controls.Add($textProfile2)
+
+    $buttonOK = CreateButton "OK" 70 110
+    $buttonOK.Add_Click({
+        Update-ProfileName -ProfileId 1 -ProfileName $textProfile1.Text
+        Update-ProfileName -ProfileId 2 -ProfileName $textProfile2.Text
+        ShowMessage "Profile names updated successfully." "OK" "Information"
+        $profileSettingsForm.Close()
+    })
+    $profileSettingsForm.Controls.Add($buttonOK)
+
+    $buttonCancel = CreateButton "Cancel" 150 110
+    $buttonCancel.Add_Click({
+        $profileSettingsForm.Close()
+    })
+    $profileSettingsForm.Controls.Add($buttonCancel)
+
+    $profileSettingsForm.ShowDialog()
+    $profileSettingsForm.Dispose()
+}
